3387c9955fb6bb322b634814fc6ccf80eabd0a03
Dominick Wiley <dman9532@gmail.com>
2023-09-07 22:43:10 -0600

    Golem now phases through most pillars (because destroying tiles is too hard apparently)

---
+++ Assets/Scripts/IgnoreCollision.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class IgnoreCollision : MonoBehaviour
+{
+    [SerializeField] private List<string> layersToIgnore; // Specify the layer(s) to ignore collisions with
+
+    private List<int> layerToIgnoreIndices; // Stores a list of layer indices to ignore collisions with
+
+    private void Start()
+    {
+        if (layersToIgnore.Count == 0) {
+            return;
+        }
+
+        // Get the layer index of the layer(s) to ignore
+        layerToIgnoreIndices = new List<int>();
+
+        foreach (string layer in layersToIgnore) {
+            layerToIgnoreIndices.Add(LayerMask.NameToLayer(layer));
+        }
+
+        foreach (int layerIndex in layerToIgnoreIndices) 
+        {            
+            if (layerIndex == -1)
+            {
+                Debug.LogError("Layer '" + layerIndex + "' not found. Make sure the layer name is correct.");
+                return;
+            }
+
+            // If the layer exists, ignore collisions with it and the entity
+            Physics2D.IgnoreLayerCollision(gameObject.layer, layerIndex, true);
+        }
+    }
+
+    // Optionally, you can add a method to re-enable collisions if needed
+    public void EnableCollisionWithLayer(string layerName)
+    {
+        int layerIndex = LayerMask.NameToLayer(layerName);
+        Physics2D.IgnoreLayerCollision(gameObject.layer, layerIndex, false);
+    }
+}
